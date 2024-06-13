0bb9e98ac09256f24da15da04c083e5cbad3b41e
Alex villada <avillaberrio@gmail.com>
2024-06-13 17:04:09 -0500

    form validation

--- JavaScript_DOM_Eventos_inicio/js/app.js
+++ JavaScript_DOM_Eventos_inicio/js/app.js
@@ -1,76 +1,164 @@
+// Seleccionar elementos...
 
-// como seleccionar contenido- las tres mas comunes son: queryselector - queryselectorAll - getelemetById
+// querySelector
 
-//  queryselector- te puede retornar ninguno hasta algun elemento  que concuerde con el selector que estas escribiendo
+const textoHeading = document.querySelector('.header__texto h2');
+console.log(textoHeading);
 
+textoHeading.textContent = 'Nuevo Heading'; // También se puede utilizar .text
 
-const heading = document.querySelector('.header__texto h2') // 0 o 1 elementos
-heading.textContent = "Nuevo Heading";
-console.log(heading)
- 
-// queryselectorAll -- retorna cero hasta todos los elementos que concuerden con el selector que estas escribiendo
+// querySelectorAll
+const enlaces = document.querySelectorAll('.navegacion a');
+// console.log(enlaces);
 
-const enlaces = document.querySelectorAll('.navegacion a')  // retorna a todos los que concuerde con el selector
-enlaces[0].textContent = "Texto";
+// Algunas operaciones...
+
+// Cambiar el texto
+enlaces[0].textContent = 'Nuevo Texto enlace';
+
+// Cambiar el enlace del primer enlace
+enlaces[0].href = 'google.com';
+
+// Agregar una clase...
 enlaces[0].classList.add('nueva-clase');
-// enlaces[0].classList.remove('navegacion__enlace')
+
+// Eliminar una clase...
+// enlaces[0].classList.remove('navegacion__enlace');
 
 
-// generar texto Html con createElement
+// Generar HTML desde JavaScript...
+const nuevoEnlace = document.createElement('A');
 
+// console.log(nuevoEnlace);
 
+// Un enlace tiene una clase...
+nuevoEnlace.classList.add('navegacion__enlace');
 
-//  generar un nuevo enlace crear HTML con JS
+// Tiene un href
+nuevoEnlace.href = 'nuevo-enlace.html';
 
-const Nuevoenlace = document.createElement('A')
-// generar un nuevo enlace
+// Tiene un Texto...
+nuevoEnlace.textContent = 'Un Nuevo Enlace';
 
- Nuevoenlace.href = 'nuevo enlace'
-// agregar el texto
+// console.log(nuevoEnlace);
 
- Nuevoenlace.textContent = 'Alex enlace';
+// Finalmente se agrega donde lo deseamos colocar...
 
-// agregar la clase
-
-Nuevoenlace.classList.add('navegacion__enlace');
-
-
-// agregarlo al documento
-
+// Tienes que seleccionar el elemento padre
 
 const navegacion = document.querySelector('.navegacion');
-navegacion.appendChild(Nuevoenlace);
-console.log(Nuevoenlace);
+navegacion.appendChild(nuevoEnlace);
 
-// eventos
+// console.log(newEnlace) 
+// Eventos en JavaScript...
 
-console.log(1)
+// Hay muchos eventos ocurriendo en tus sitios y aplicaciones web, cuando un usuario da click, cuando dan scroll, al presionar en un botón, enviar un formulario, pero uno de los más comunes es esperar a que el documento este listo...
 
 
-window.addEventListener('load', function(){ // load espera a que el js y los archivos que depende del HTML esten listos
-    console.log(2)
-});
 
-window.onload = function(){
-    console.log(3)
-}
+// console.log('1');
+// window.addEventListener('load', function() { // Cuando el Archivo JS y los archivos dependientes han cargado como es el HTML y las imagenes...
+//     console.log('2');
+// });
 
-document.addEventListener('DOMContentLoaded', function(){
-     console.log(4)
-})
-console.log(5)  
+// window.onload = function() {
+//     console.log('3')
+// };
 
-window.onscroll = function(){
-    console.log('scrolling...')
-}
+// document.addEventListener('DOMContentLoaded', function() { // Este se ejecuta cuando el HTML ha sido descargado pero no espera por CSS o imagenes...
+//     console.log('4');
+// });
+
+// console.log('5');
+
+// Estos closures también pueden ser con una función aparte...
 // cual es la diferencia entre Load y DOMContentLoaded - en el caso de Load espera a que se descargue todo el HTML,imagenes etc y el DOMContenLoad solo espera a que se descargue el HTML no espera Css o img
 
-// seleccionar elementos y asociarles eventos
+// // Eventos Scroll...
+// window.onscroll = function(e) {
+//     console.log('scrolling...');
 
-const btnenviar = document.querySelector('.boton--primario')
-btnenviar.addEventListener('click', function(evento){
-    console.log(evento)
-    evento.preventDefault()
-    console.log('Enviando formulario');
+//     console.log(this.scrollY);
+// }
+
+
+
+// Eventos con Click y submit...
+
+// const btnEnviar = document.querySelector('.formulario input[type=submit]');
+// console.log(btnEnviar);
+
+// btnEnviar.addEventListener('click', function() { // callback o closure 
+//     console.log('click');
+// });
+
+
+const data = {
+  nombre: '',
+  email: '',
+  mensaje: ''
+}
+
+// submit
+
+const formulario = document.querySelector('.formulario');
+formulario.addEventListener('submit', function(e) {
+   e.preventDefault();
+   
+   console.log(e);
+
+   console.log('Di click y la página ya no recarga');
+
+   console.log(data);
+
+   // Validar el formulario...
+   // destructuring
+   const {nombre, email, mensaje} = data;
+   if( nombre === '' || email === '' || mensaje === ''){
+      console.log(' Al menos un campo está vacío');
+      mostrarError('Todos los campos son obligatorios');
+      return; // Detiene la ejecución del código...
+   }
+   console.log('Todo bien...');
+   mostrarCorrecto('Mensaje enviado correctamente');
 })
 
+function mostrarError(mensaje){
+  const alerta = document.createElement('P')
+  alerta.textContent = mensaje;
+  alerta.classList.add('error');
+
+  formulario.appendChild(alerta);
+
+  setTimeout(() => {
+    alerta.remove();
+  }, 4000);
+}
+
+function mostrarCorrecto(mensaje) {
+  const alerta = document.createElement('p');
+  alerta.textContent = mensaje;
+  alerta.classList.add('correcto');
+  formulario.appendChild(alerta);
+
+  setTimeout(() => {
+      alerta.remove();
+  }, 3000);
+}
+
+// Eventos de los Inputs...
+
+
+
+nombre.addEventListener('input', leerTexto);
+email.addEventListener('input', leerTexto);
+mensaje.addEventListener('input', leerTexto);
+
+function leerTexto(e) {
+    // console.log(e);
+    // console.log(e.target.value);
+
+   data[e.target.id] = e.target.value;
+
+    console.log(data);
+}
